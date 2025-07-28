f086564cf565741cab249f247b2ef4c2e2d55d5b
Luis Santiago Aguilar Ulloa <aguilarulloaluis@gmail.com>
2025-07-28 18:48:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,50 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from PIL import Image
 
+# Leer los datos
 car_data = pd.read_csv('vehicles_us.csv') 
 
-# Encabezado
-st.header('DATOS DE VEHICULOS COMERCIALES')
+# Configuración de la página
+img = Image.open('logo.png')
+st.set_page_config(page_title='Auto Cars TRIPLETEN', page_icon=img)
 
-# Aqui se muestra los primeros datos de vehiculos
-if st.checkbox('Mostrar los primeros datos de la lista de vehiculos'):
+# Encabezado
+st.header('DATOS DE VEHÍCULOS COMERCIALES')
+
+# Mostrar los primeros datos
+if st.checkbox('Mostrar los primeros datos de la lista de vehículos'):
     st.write(car_data.head(10))
 
-
-if st.button('Histograma de Vehiculos por kilometraje recorrido'):
-    st.write('Histograma de la columna odómetro o kilometraje recorrido')
+# Botón: Histograma por kilometraje
+if st.button('Histograma de vehículos por kilometraje recorrido'):
+    st.write('Histograma de la columna "odometer" (kilometraje recorrido)')
     fig = px.histogram(car_data, x='odometer')
     st.plotly_chart(fig)
 
-if st.button('Diagrama de Dispersión de Vehiculos'):
-    st.write('Diagrama de dispersión entre: Kilometraje vs Precio')
+# Botón: Diagrama de dispersión
+if st.button('Diagrama de dispersión de vehículos'):
+    st.write('Dispersión entre kilometraje (odometer) y precio')
     fig2 = px.scatter(car_data, x='odometer', y='price')
     st.plotly_chart(fig2)
-    
-# Versiónes  con checkboxes
 
-if st.checkbox('CLICK PARA MOSTRAR EL HISTOGRAMA POR KILOMETRO RECORRIDO'):
-    st.write('Histograma de Vehiculos por Kilometraje recorrido')
+# Checkbox: Mostrar histograma
+if st.checkbox('Mostrar histograma por kilometraje recorrido'):
+    st.write('Histograma de vehículos por kilometraje recorrido')
     fig3 = px.histogram(car_data, x='odometer')
     st.plotly_chart(fig3)
 
-if st.checkbox('CLICK PARA MOSTRAR LA DISPERSION ENTRE: KILOMETRAJE Y PRECIO'):
-    st.write('Diagrama de dispersión de Vehiculos entre: kilometraje vs precio')
+# Checkbox: Mostrar dispersión
+if st.checkbox('Mostrar dispersión entre kilometraje y precio'):
+    st.write('Dispersión entre kilometraje (odometer) y precio')
     fig4 = px.scatter(car_data, x='odometer', y='price')
     st.plotly_chart(fig4)
+
+
+
+# Mi_proyecto_sprint_7
+# En la aplicación es importa
 
 
 
